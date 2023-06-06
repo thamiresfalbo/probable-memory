9b6bb480032ed7ffdb03362137af058eab6fc131
Thamires Falbo <111710331+thamiresfalbo@users.noreply.github.com>
2023-06-06 19:35:56 -0300

    Added dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   resources :users
   resources :user_sessions, only: %i[new create destroy]
   root 'home#index'
-  
+
   get 'login', to: 'user_sessions#new', as: :login
   post 'login', to: 'user_sessions#create'
   post 'logout', to: 'user_sessions#destroy', as: :logout
+
+  get 'dashboard', to: 'dashboard#show', as: :dashboard
 end
