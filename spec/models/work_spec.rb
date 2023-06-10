1c85889c64c176893bcce5884e838c2483ac16e5
Thamires Falbo <111710331+thamiresfalbo@users.noreply.github.com>
2023-06-10 01:26:49 -0300

    Cooked some initial tests.

--- spec/models/work_spec.rb
+++ spec/models/work_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
-
+# TODO: Add Factory_Bot
 RSpec.describe Work, type: :model do
+  context 'Work' do
+    let(:work) { Work.new }
+    it 'should not have empty tags' do
+      expect(:works.tag_list.empty?).to be false
+    end
+
+    it 'should have title' do
+    end
+
+    it 'should have more than three tags' do
+      expect(:works.tag_list.count).to be >= 3
+    end
+
+    it 'should have an author' do
+    end
+  end
 end
